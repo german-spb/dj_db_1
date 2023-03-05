9202c530c89dd0120e5450ab7b33202b6c39f84f
Герман <108299345+german-spb@users.noreply.github.com>
2023-03-05 18:52:43 +0300

    Update models.py

--- phones/models.py
+++ phones/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.urls import reverse
+
 
 class Phone(models.Model):
     name = models.CharField(max_length=50, null=False)
@@ -9,6 +9,5 @@ class Phone(models.Model):
     lte_exists = models.BooleanField()
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL', blank=True)
 
-    # def get_absolute_url(self):
-    #     return reverse('phone', kwargs={'slug': self.slug})
+   
 
