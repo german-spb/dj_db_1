5beaff22173d6959b0c9d7c04a2092f5fabaca77
Герман <108299345+german-spb@users.noreply.github.com>
2023-03-05 18:49:55 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -53,7 +53,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'phones_db',
         'USER': 'german',
-        'PASSWORD': 'gkoretskiy27',
+        'PASSWORD': '******',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
